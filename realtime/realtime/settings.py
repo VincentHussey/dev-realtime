4768deefcb9b3ec9f950f2ddd143640a28be7d43
VincentHussey <vincent.hussey@opw.ie>
2012-09-12 13:02:37 +0100

    Add south & dev_realtime to applications.

--- realtime/realtime/settings.py
+++ realtime/realtime/settings.py
@@ -134,7 +134,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    # '',
+    'south',
+    'dev_realtime',
 )
 
 # A sample logging configuration. The only tangible logging
