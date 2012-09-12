c6f96a7d0d6b087588a84d81efcca8efc5dbbd2e
VincentHussey <vincent.hussey@opw.ie>
2012-09-12 13:35:17 +0100

    Correct typo.

--- utils/create_database.sh
+++ utils/create_database.sh
@@ -7,6 +7,6 @@ createdb -U postgres -E UTF8 -O $USER $DB_NAME
 createlang -U postgres plpgsql $DB_NAME
 psql -U postgres -d $DB_NAME -f /usr/share/pgsql/contrib/postgis.sql
 psql -U postgres -d $DB_NAME -f /usr/share/pgsql/contrib/spatial_ref_sys.sql
-psql -U postgres -d $DB_NAME -c "alter table spatial_ref_sys owner to flood"
-psql -U postgres -d $DB_NAME -c "alter table geometry_columns owner to flood"
-psql -U postgres -d $DB_NAME -c "alter table geography_columns owner to flood"
+psql -U postgres -d $DB_NAME -c "alter table spatial_ref_sys owner to $USER"
+psql -U postgres -d $DB_NAME -c "alter table geometry_columns owner to $USER"
+psql -U postgres -d $DB_NAME -c "alter table geography_columns owner to $USER"
